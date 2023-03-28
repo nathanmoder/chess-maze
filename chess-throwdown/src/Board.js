bf86983cc241c7f3874d618d6e090554edd2cb9f
nathanmoder <63596741+nathanmoder@users.noreply.github.com>
2023-03-27 21:57:12 -0400

    implemented game initialization in Board.js.

--- chess-throwdown/src/Board.js
+++ chess-throwdown/src/Board.js
@@ -1,4 +1,8 @@
-import React from 'react'
+//Nathan Moder
+//Last edited 3/27/2023
+
+
+import React, {useState} from 'react'
 
 
 /*
@@ -13,9 +17,49 @@ detecting game overs, and allowing for piece movement.
 */
 
 function Board() {
+    //pieces will store an array of objects with four attributes.
+        //it will later be augmented by the addPiece and initializeBoard functions
+    const [pieces, setPieces]=useState([])
+
+    //keeps track of whether or not the game has begun, so that initializeBoard
+        //is not called excessively
+    const [hasStarted,setHasStarted]=useState(false)
+
+    //keeps track of what stage the player is on. the board will get
+        //progressively larger as the player clears stages
+    const [stageNumber,setStageNumber]=useState(1);
+
+    //INPUT: Attributes equivalent to those of the object in pieces
+    //Adds a new element to the array pieces
+    //RETURN: None
+    const addPiece = (_x,_y,_pieceType,_allegiance) => {
+        const _blackwhite=_x+_y%2==0 ? 'white' : 'black';
+        setPieces([pieces,{pieceType:_pieceType,allegiance:_allegiance,blackwhite:_blackwhite,position:[_x,_y]}])
+    }
+
+    //INPUT: Size of the board to be instantiated
+    //After executing, pieces will be full of squares both without and containing pieces
+    //RETURN: None
+    const initializeBoard = (size) => {
+        for(let i=0; i<size; i++){
+            for(let j=0; j<size; j++){
+                addPiece(i,j,'none','none')
+            }
+        }
+    }
+
+    //Initializes the board if this has not yet been done, or if a new stage is starting
+    if(!hasStarted){
+        initializeBoard((stageNumber<5 ? stagenumber+3 : 8))
+        setHasStarted(true)
+    }
     return (
-        <div>
-            Board
+        <div id="board">
+            {pieces.map(
+            e => {
+                (<div id={position[0]+position[1]} className={e.allegiance+e.blackwhite+" square"}>{e.pieceType}</div>)
+            }
+        )}
         </div>
     )
 }
