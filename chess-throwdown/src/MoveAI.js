8475a5562c8c877b66e3c9ef9c18b5a0af2c3d87
nathanmoder <63596741+nathanmoder@users.noreply.github.com>
2023-04-03 15:24:19 -0400

    implemented movePiece in MoveAI

--- chess-throwdown/src/MoveAI.js
+++ chess-throwdown/src/MoveAI.js
@@ -4,13 +4,14 @@ import PieceMovement from "./PieceMovement";
 //Algorithm uses alpha-beta pruning of 
 //RETURN: An quadruplet [oldx,oldy,newx,newy] that describes which
     //piece should be moved to which location.
-function MoveAI(difficulty, squares){
+function MoveAI(difficulty, board){
 
     //Used in the Alpha-Beta-Pruning algorithm
     let alpha=-100000;
     let beta=100000;
 
     //Credit to the chess programming wiki at https://www.chessprogramming.org/Simplified_Evaluation_Function
+        //for the values of pieces and the location matrices.
     //RETURN: The value(for enemy pieces) of the piece at x,y
     const valuePieceAt = (x,y) =>{
 
@@ -25,18 +26,19 @@ function MoveAI(difficulty, squares){
 
     //Updates the board with the new move
     //RETURN: None
-    const movePiece = (move) =>{
-
+    const movePiece = (move,_type,_allegiance) =>{
+        board[move[1][0]+move[1][1]]=board[move[0][0]+move[0][1]];
+        board[move[0][0]+move[0][1]]={ pieceType: _type, allegiance: _allegiance, position: [move[0][0], move[0][1]] };
     }
 
 
     //RETURN: Pairs of coordinates corresponding to every possible move by the given player
     const getAllMoves = (_allegiance)=>{
-        moves=[];
+        let moves=[];
         for(let x=0; x<8; x++){
             for(let y=0; y<8; y++){
-                if(squares[x+8*y].allegiance==_allegiance){
-                    movesToAdd=PieceMovement(squares[x+8*y].pieceType,x,y,squares);
+                if(board[x+8*y].allegiance==_allegiance){
+                    movesToAdd=PieceMovement(board[x+8*y].pieceType,x,y,board);
                     for(let move of movesToAdd){
                         moves.push([[x,y],[move]]);
                     }
@@ -47,14 +49,15 @@ function MoveAI(difficulty, squares){
 
     }
 
-    //Credit to the chess programming wiki at https://www.chessprogramming.org/Alpha-Beta
+    //Augmented from the template at https://www.chessprogramming.org/Alpha-Beta
     const alphaBetaMax = (a,b,depthLeft) =>{
         if(depthLeft==0)
             return valuation();
         for(let move of getAllMoves('e')){
-            movePiece([move[0],move[1]]);
+            oldPiece=board[[move[1][0]+8*move[1][1]]];
+            movePiece([move[0],move[1]],'none','none');
             score=alphaBetaMin(a,b,depthLeft-1);
-            movePiece([move[1],move[0]]);
+            movePiece([move[1],move[0]],oldPiece.pieceType,oldPiece.allegiance);
             if(score >= b)
                 return b;
             if(score > a)
@@ -67,9 +70,10 @@ function MoveAI(difficulty, squares){
         if(depthLeft==0)
             return valuation();
         for(let move of getAllMoves('p')){
-            movePiece([move[0],move[1]]);
+            oldPiece=board[[move[1][0]+8*move[1][1]]];
+            movePiece([move[0],move[1]],'none','none');
             score=alphaBetaMax(a,b,depthLeft-1)[0];
-            movePiece([move[1],move[0]]);
+            movePiece([move[1],move[0]],oldPiece.pieceType,oldPiece.allegiance);
             if(score <= a)
                 return a;
             if(score < a)
@@ -79,7 +83,7 @@ function MoveAI(difficulty, squares){
 
     }
 
-    return alphaBetaMax[1];
+    return alphaBetaMax(alpha,beta,difficulty)[1];
 }
 
 export default MoveAI;
